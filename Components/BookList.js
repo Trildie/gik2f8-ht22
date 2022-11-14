82435c2554f49747a849f3b5e15c93ff8da1179a
Oskar Sjödin <oskar.s@hotmail.com>
2022-11-14 17:45:32 +0100

    Komponenter

---
+++ Components/BookList.js
@@ -0,0 +1,14 @@
+const BookList = (bookList) => {
+    console.log(BookList);
+    
+
+    let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`
+    for(let i = 0; i < bookList.length; i++){
+        html += BookListItem(bookList[i]);
+    }                
+
+    html += `</ul>`;
+
+    return html;
+
+};
